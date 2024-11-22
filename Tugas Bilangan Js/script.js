8b902e56b72d5c92e10076fb649fc2049031e915
Viepeir <vikramakat123@gmail.com>
2024-11-22 13:50:04 +0700

    first commit

---
+++ Tugas Bilangan Js/script.js
@@ -0,0 +1,9 @@
+// Mendeklarasikan dua bilangan dengan let
+let bilangan1 = 5;
+let bilangan2 = 10;
+
+// Menggunakan const untuk mendeklarasikan hasil perkalian
+const hasilPerkalian = bilangan1 * bilangan2;
+
+// Menampilkan hasil
+console.log("Hasil perkalian:", hasilPerkalian);
